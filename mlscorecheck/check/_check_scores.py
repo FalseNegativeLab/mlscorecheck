b9907d1810cdf2089ae98bfb617c68b9ef3db89b
György Kovács <gyorgy.kovacs@daalab.com>
2023-08-18 09:35:03 +0200

    problem validation added

--- mlscorecheck/check/_check_scores.py
+++ mlscorecheck/check/_check_scores.py
@@ -14,7 +14,9 @@ __all__ = ['_resolve_p_n',
             'check_kfold_mor_scores',
             'check_multiple_datasets_mor_scores',
             'check_multiple_datasets_mor_kfold_rom_scores',
-            'check_multiple_datasets_mor_kfold_mor_scores']
+            'check_multiple_datasets_mor_kfold_mor_scores',
+            '_check_problem_specification',
+            'validate_problem_specification']
 
 def _resolve_p_n(dataset_conf):
     """
@@ -39,6 +41,24 @@ def _resolve_p_n(dataset_conf):
 
     return result
 
+def _check_problem_specification(problem):
+    """
+    Check the problem specification
+    """
+    if isinstance(problem, dict):
+        if not (('p' in problem and 'n' in problem) or ('dataset' in problem) or ('fold_configuration' in problem)):
+            return False
+        if 'fold_configuration' in problem:
+            if not all('p' in fold and 'n' in fold for fold in problem['fold_configuration']):
+                return False
+        return True
+
+    return all(_check_problem_specification(prob) for prob in problem)
+
+def validate_problem_specification(problem):
+    if not _check_problem_specification(problem):
+        raise ValueError('the problem specification is incorrect')
+
 def check_scores(scores,
                     eps,
                     dataset,
@@ -47,6 +67,8 @@ def check_scores(scores,
     """
     Use for one dataset - based on the solution formulas
     """
+    validate_problem_specification(dataset)
+
     dataset = _resolve_p_n(dataset)
 
     results = check(scores, eps=eps, p=dataset['p'], n=dataset['n'])
@@ -64,6 +86,8 @@ def check_kfold_rom_scores(scores,
     """
     Use when scores are aggregated in the RoM manner
     """
+    validate_problem_specification(dataset)
+
     dataset_resolved = _resolve_p_n(dataset)
     dataset_resolved['p'] = dataset_resolved['p'] * dataset_resolved['n_repeats']
     dataset_resolved['n'] = dataset_resolved['n'] * dataset_resolved['n_repeats']
@@ -78,6 +102,8 @@ def check_multiple_datasets_rom_scores(scores,
                                         datasets,
                                         *,
                                         return_details=False):
+    validate_problem_specification(datasets)
+
     datasets = _resolve_p_n(datasets)
     tmp = {'p': sum(dataset['p'] for dataset in datasets),
             'n': sum(dataset['n'] for dataset in datasets)}
@@ -95,6 +121,8 @@ def check_multiple_datasets_rom_kfold_rom_scores(scores,
     """
     Use when scores are aggregated in the RoM manner
     """
+    validate_problem_specification(datasets)
+
     datasets = _resolve_p_n(datasets)
     tmp = {'p': sum(dataset['p']*dataset['n_repeats'] for dataset in datasets),
             'n': sum(dataset['n']*dataset['n_repeats'] for dataset in datasets)}
@@ -115,6 +143,8 @@ def check_kfold_mor_scores(scores,
     By default, assumes stratified k-fold. If fold_setup is provided,
     it does overwrite all other arguments.
     """
+    validate_problem_specification(dataset)
+
     dataset = _resolve_p_n(dataset)
     return consistency_1(scores=scores,
                             eps=eps,
@@ -126,6 +156,8 @@ def check_multiple_datasets_mor_scores(scores,
                                         datasets,
                                         *,
                                         return_details=False):
+    validate_problem_specification(datasets)
+
     datasets = _resolve_p_n(datasets)
     return check_kfold_mor_scores(scores=scores,
                                     eps=eps,
@@ -137,6 +169,8 @@ def check_multiple_datasets_mor_kfold_rom_scores(scores,
                                                 datasets,
                                                 *,
                                                 return_details=False):
+    validate_problem_specification(datasets)
+
     fold_configuration = []
 
     for dataset in datasets:
@@ -164,6 +198,8 @@ def check_multiple_datasets_mor_kfold_mor_scores(scores,
                                                 datasets,
                                                 *,
                                                 return_details=False):
+    validate_problem_specification(datasets)
+
     datasets = _resolve_p_n(datasets)
 
     return consistency_grouped(scores=scores,
---
+++ tests/check/test_problem_validation.py
@@ -0,0 +1,20 @@
+"""
+This module tests the problem validation
+"""
+
+import pytest
+
+from mlscorecheck.check import validate_problem_specification
+
+def test_validate_problem_specification():
+    """
+    Testing the problem validation
+    """
+    with pytest.raises(ValueError):
+        validate_problem_specification({})
+
+    with pytest.raises(ValueError):
+        validate_problem_specification([{}])
+
+    with pytest.raises(ValueError):
+        validate_problem_specification([{'fold_configuration': [{}]}])
